bb4c39c057fc3ab57a60b7d72e8c8f951c9a6b05
Hong Ming <hongming2000@gmail.com>
2021-04-24 16:08:11 +0800

    Added Character and UI to combined Scenes
    You can now pull the file and edit immediately through Unity.

---
+++ Combined Scenes (With Character and UI)/Assets/Game/UI/Prefabs and Scripts/EN.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EN : MonoBehaviour
+{
+    public int maxEN = 100;
+    public int currentEN;
+    public EN_bar enBar;
+
+    public Text enText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentEN = Controls.cellsCollected;
+        enBar.SetMaxEN(maxEN);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentEN = Controls.cellsCollected;
+        enText.text = "Energy : " + currentEN*20 + "%";
+        
+        enBar.SetEN(currentEN);
+        
+    }
+}
